00a29f3c5f0d4fb7069585c85d49c41d5ae67b68
So2ln <sorrinj77@snu.ac.kr>
2025-08-07 12:13:50 +0900

    feat: splash screen 제작, flutter_native_splash 설치, 로고 사진 파일 추가
    - 네이티브 스플래시 이후에 애니메이션 스플래시 페이지가 나타나도록 구현, 그리고 애니메이션이 끝나면 메인 화면으로 이동

--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -1,8 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:gps_chat_app/ui/pages/home/home_page.dart';
 
-class WelcomePage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController catMoveController;
+  late AnimationController devController;
+  late AnimationController potController;
+  late AnimationController bubbleController;
+
+  late Animation<Offset> devOffsetAnimation;
+  late Animation<double> devScaleAnimation;
+
+  late Animation<Offset> potOffsetAnimation;
+  late Animation<double> potScaleAnimation;
+
+  late Animation<Offset> bubbleOffsetAnimation;
+  late Animation<double> bubbleScaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    catMoveController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+    devController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    potController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    bubbleController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    // 애니메이션 정의
+    devOffsetAnimation =
+        Tween<Offset>(begin: const Offset(0, 0.5), end: Offset.zero).animate(
+          CurvedAnimation(parent: devController, curve: Curves.easeOutBack),
+        );
+    devScaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: devController, curve: Curves.easeOutBack),
+    );
+    potOffsetAnimation =
+        Tween<Offset>(begin: const Offset(0, 0.5), end: Offset.zero).animate(
+          CurvedAnimation(parent: potController, curve: Curves.easeOutBack),
+        );
+    potScaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: potController, curve: Curves.easeOutBack),
+    );
+    bubbleOffsetAnimation =
+        Tween<Offset>(begin: const Offset(-0.5, 0.2), end: Offset.zero).animate(
+          CurvedAnimation(parent: bubbleController, curve: Curves.easeOutBack),
+        );
+    bubbleScaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: bubbleController, curve: Curves.easeOutBack),
+    );
+
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(milliseconds: 800));
+      devController.forward();
+      await Future.delayed(const Duration(milliseconds: 300));
+      potController.forward();
+      await Future.delayed(const Duration(milliseconds: 300));
+      bubbleController.forward();
+
+      // 애니메이션 총 시간과 비슷하게 딜레이를 준 후 메인 화면으로 이동
+      await Future.delayed(const Duration(seconds: 2));
+
+      // 화면 전환
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    catMoveController.dispose();
+    devController.dispose();
+    potController.dispose();
+    bubbleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('welcome_page')));
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            bottom: screenHeight * 0.4,
+            child: Image.asset(
+              'assets/images/base_circle.png',
+              height: screenHeight * 0.05,
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.45,
+            child: AnimatedBuilder(
+              animation: catMoveController,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(0, 10 * (catMoveController.value - 0.5)),
+                  child: child,
+                );
+              },
+              child: Image.asset(
+                'assets/images/catpot.png',
+                height: screenHeight * 0.2,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SlideTransition(
+                  position: devOffsetAnimation,
+                  child: ScaleTransition(
+                    scale: devScaleAnimation,
+                    child: const Text(
+                      'dev',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 7),
+                SlideTransition(
+                  position: potOffsetAnimation,
+                  child: ScaleTransition(
+                    scale: potScaleAnimation,
+                    child: const Text(
+                      'pot',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.6,
+            right: screenWidth * 0.15,
+            child: SlideTransition(
+              position: bubbleOffsetAnimation,
+              child: ScaleTransition(
+                scale: bubbleScaleAnimation,
+                child: Image.asset(
+                  'assets/images/code_bubble.png',
+                  width: 60,
+                  height: 60,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
