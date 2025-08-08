f2778dab100b0b80765809b467fb5b7614280f53
PolarisAnnie <polariseunhee@gmail.com>
2025-08-08 14:13:24 +0900

    feat: 채팅방 UI 추가

--- lib/ui/pages/chat/chat_page.dart
+++ lib/ui/pages/chat/chat_page.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:gps_chat_app/data/model/chat_message.dart';
+import 'package:gps_chat_app/ui/pages/chat/widgets/chat_bottom_sheet.dart';
+import 'package:gps_chat_app/ui/pages/chat/widgets/chat_recieve_item.dart';
+import 'package:gps_chat_app/ui/pages/chat/widgets/chat_send_item.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  List<ChatMessage> messages = [];
+  void onSendMessage(String content) {
+    // 새 메시지 추가 함수
+    setState(() {
+      messages.add(
+        ChatMessage(
+          chatId: 2, //TODO: 자동 생성
+          content: content,
+          createdAt: DateTime.now(),
+          senderId: 1, //TODO: 유저 아이디로 연결
+          messageType: '', // TODO: 내가 보낸 메시지
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('chat_page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('받는 사람 이름')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          //TODO: 하드코딩된 부분 수정 및 메시지 업로드 시 화면 업데이트 필요함
+          children: [
+            SizedBox(height: 10),
+            ChatSendItem(
+              imageUrl: '',
+              content: '안녕하세요안녕하세요안녕안녕안녕안녕안녕안녕안녕안녕안녕',
+              dateTime: DateTime.now(),
+            ),
+            SizedBox(height: 14),
+            ChatReceiveItem(
+              imageUrl: '',
+              content: '안녕하세요안녕하세요안녕안녕안녕안녕안녕안녕안녕안녕안녕',
+              dateTime: DateTime.now(),
+            ),
+            SizedBox(height: 10),
+            ChatReceiveItem(
+              imageUrl: '',
+              content: '반가워염',
+              dateTime: DateTime.now(),
+            ),
+            SizedBox(height: 14),
+            ChatSendItem(
+              imageUrl: '',
+              content: '안녕하세요안녕하세요안녕안녕안녕안녕안녕안녕안녕안녕안녕',
+              dateTime: DateTime.now(),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: ChatBottomSheet(
+        MediaQuery.of(context).padding.bottom,
+        onSendMessage,
+      ),
+    );
   }
 }
---
+++ lib/ui/pages/chat/widgets/chat_recieve_item.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ChatReceiveItem extends StatelessWidget {
+  ChatReceiveItem({
+    required this.imageUrl,
+    required this.content,
+    required this.dateTime,
+  });
+
+  final String imageUrl;
+  final String content;
+  final DateTime dateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 32,
+          width: 32,
+          //decoration: BoxDecoration(shape: BoxShape.circle),
+          child: GestureDetector(
+            onTap: () {
+              print('프로필 이미지 클릭');
+            },
+            child: ClipOval(
+              child: Image.network(
+                //TODO : firebase 프로필 이미지 URL로 연결
+                'https://picsum.photos/320/320',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 8),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '닉네임',
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 3),
+              Padding(
+                padding: const EdgeInsets.only(right: 60),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text(content, style: TextStyle(fontSize: 13)),
+                ),
+              ),
+              SizedBox(height: 3),
+              Text(
+                dateTime.toIso8601String(),
+                style: TextStyle(fontSize: 11, color: Colors.black45),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
